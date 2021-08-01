158c84d8300a75b767a2fd1b5e075312368d7251
thrace92i <pierre.fournier42@gmail.com>
2021-08-01 12:14:32 +0200

    Création BDD + connexion mongoose

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+// Méthode pour tout type de requête
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
 
+
+mongoose.connect('mongodb+srv://sopekockoadm:Dw7bgMAvhHpxi1e4lXxk@cluster0.1vowr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 module.exports = app;
+
+
+
