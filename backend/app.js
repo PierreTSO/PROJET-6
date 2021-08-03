d06c520c318dcb27be3e3852a6cff9e3f7621bca
thrace92i <pierre.fournier42@gmail.com>
2021-08-03 15:17:02 +0200

    Correction parcours utilisateur

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const path = require('path');
+
+const app = express();
+
 const mongoose = require('mongoose');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
+const Sauce = require('./models/Sauce')
 const userRoutes = require('./routes/user');
 
-const Sauce = require('./models/Sauce')
+
 
 mongoose.connect('mongodb+srv://sopekockoadm:Dw7bgMAvhHpxi1e4lXxk@cluster0.1vowr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -15,7 +21,6 @@ mongoose.connect('mongodb+srv://sopekockoadm:Dw7bgMAvhHpxi1e4lXxk@cluster0.1vowr
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const app = express();
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); //Header permettant d'accéder à notre API depuis n'importe quelle origine
@@ -30,21 +35,21 @@ app.use(bodyParser.json());
 app.use('/api/auth', userRoutes);
 
 //Route pour poster une sauce sur l'application
-app.post('/api/sauces', (req, res, next) => {
-  delete req.body._id;
-  const sauces = new Sauce({
-    name: req.body.name,
-    imageUrl: req.body.imageUrl
-  });
-  sauces.save()
-    .then(() => res.status(201).json({ message: 'Votre sauce a été enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+// app.post('/api/sauces', (req, res, next) => {
+//   delete req.body._id;
+//   const sauces = new Sauce({
+//     name: req.body.name,
+//     imageUrl: req.body.imageUrl
+//   });
+//   sauces.save()
+//     .then(() => res.status(201).json({ message: 'Votre sauce a été enregistré !'}))
+//     .catch(error => res.status(400).json({ error }));
+// });
 
 // Méthode pour tout type de requête
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+// app.use((req, res) => {
+//    res.json({ message: 'Votre requête a bien été reçue !' }); 
+// });
 
 
 module.exports = app;
